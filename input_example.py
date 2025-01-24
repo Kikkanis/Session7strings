6f4d62e59eefa31ba1fde4e040d268c9d24dacf9
kikkan <kristian.berger6@gmail.com>
2025-01-24 11:25:49 +0100

    input examples

---
+++ input_example.py
@@ -0,0 +1,15 @@
+name = input("what is your name? ")
+
+
+
+
+
+# lets try this in a simpler way
+age2 = input(f"how old are you {name}? ")
+age2 = int(age2)
+print(f"{name}, you were born in {2024-age2}")
+
+# SYntax errors are not pycharm /python language at all
+# Exception errors are errors we can do some edits to, so its not all wrong.
+
+
