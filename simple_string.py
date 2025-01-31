d44998412c21ee56f39c0a797b21adbcf3280c9b
kikkan <kristian.berger6@gmail.com>
2025-01-31 11:24:10 +0100

    added simple string

---
+++ simple_string.py
@@ -0,0 +1,19 @@
+s = ("hello")
+print(s)
+
+s2 =  "Hello sir wefjiwejfiwefij"
+print(s2)
+
+s3 = ('And she said: "hello!')
+print (s3)
+
+print(",.'\"\"\"\"")
+
+# here we print a certain letter in the word, using the commands below
+s = "hello"
+print(s[2], s[4])     # you index by position, first index is 0. this is to print first l in "hello
+print(s[-1])   # go around to the end!
+print(s[1], s[-4])
+
+
+
