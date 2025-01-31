bb4ec4a91af2acda3d4b842ac0ca834cd5423b33
kikkan <kristian.berger6@gmail.com>
2025-01-31 12:27:42 +0100

    added string slices

---
+++ slices_strings.py
@@ -0,0 +1,18 @@
+s = "abcdefghiklmnop"
+print(s)
+print(s[1:4], s[6:9])
+print(s[:4], s[6:])
+print(s[1:10:2])
+print(s[::-1])
+print("racecar"[::-1]) # racecar is racecar in reverse (we reverse her with -1)
+
+
+# this is how you replace a letter
+
+s = "cat"
+s = "r" + s[1:]
+print(s)
+
+s = "seven" # i want to change it to seven
+s = s[:2] + "7" + s[3:]
+print(s)
