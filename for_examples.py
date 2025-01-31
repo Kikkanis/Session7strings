3ef655568b251c9767501aeca8e41bc7b0c3db09
kikkan <kristian.berger6@gmail.com>
2025-01-31 11:05:51 +0100

    Added for examples

---
+++ for_examples.py
@@ -0,0 +1,18 @@
+# sequence of 1 to 10 with iterations of 2
+for i in range(1, 10, 2):
+    print(i)
+s = 0
+i = 1
+while i <= 6:
+    print(i)
+
+for i in range(1, 11):
+    s += i
+    i += 1
+
+    # s = s + i
+
+print("sum using while is also", s)
+
+
+
